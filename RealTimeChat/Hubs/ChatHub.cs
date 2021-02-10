e583146944cba745e4e5a6754aa8f7f004c0ac3b
Alain Hsu <alain.hsu@outlook.com>
2021-02-10 21:37:26 +0800

    Initial commit

---
+++ RealTimeChat/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace RealTimeChat.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("receiveConnId",Context.
+                ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task receiveMsg(String msg)
+        {
+            MessageTemplate temp = JsonConvert.DeserializeObject<MessageTemplate>(msg);
+            string toWhom = temp.toWhom; /// userId --> connId (from DB)
+
+            await Clients.Client(toWhom).SendAsync("receiveMsg",temp.message);
+
+            //var client = Clients.AllExcept(temp.FromWhom);
+            //await client.SendAsync("receiveMsg", temp.Message);
+        }
+    }
+}
---
+++ RealTimeChat/Hubs/MessageTemplate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RealTimeChat.Hubs
+{
+    public class MessageTemplate
+    {
+        public string toWhom { get; set; }
+        public string fromWhom { get; set; }
+        public string message { get; set; }
+    }
+}
